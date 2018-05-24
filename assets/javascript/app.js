fd08d69f7f613a526ce7e068263214aab6044ff8
ATotti <amirhasan.jafari@philips.com>
2018-05-24 01:31:28 -0400

    good code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,23 @@
 var correctAs = 0;
 var wrongAs = 0;
-var number = 10;
+var number = 15;
 var intervalId;
 
-var questions = ["#first-question", "#second-question", "#third-question"];
-var answers = ["France", "Klose", "Brazil"];
+var questions = ["#q1", "#q2", "#q3", "#q4", "#q5", "#q6"];
+var answers = ["Italy", "Ronaldo", "Brazil", "Klose", "Uruguay", "USA"];
+var images = ["assets/images/zidane.gif", "assets/images/ronaldo.gif", "assets/images/brazil.gif", "assets/images/klose.gif", "assets/images/uruguay.gif", "assets/images/usa.gif"]
 var j = 0;
 
 //Setting timer
 
 $(document).ready(function () {
 
+    $("#startbtn").on("click", function () {
+        runTimer();
+        $(questions[0]).show();
+        $("#startbtn").hide();
+    })
 
-    runTimer();
-    $(questions[0]).show();
     //click function to submit the answer
     $(".choice").on("click", function () {
 
@@ -22,10 +26,10 @@ $(document).ready(function () {
         //debugger;
         if (userAnswer === answers[j]) {
             correctAs++
-            console.log("correct answers: " + correctAs);
+            //console.log("correct answers: " + correctAs);
             //alert("Answer is correct!");
 
-            $(questions[j]).html("<p>You were correct!</p><img src='assets/images/crystal2.jpg'/>")
+            $(questions[j]).html("<h4>You were correct!</h4><img src=" + images[j] + " width='300px'>")
             setTimeout(replaceFunction, 2000);
             setTimeout(showNext, 3000);
         }
@@ -33,20 +37,21 @@ $(document).ready(function () {
 
         else {
             wrongAs++
-            console.log("wrong answers: " + wrongAs);
+            //console.log("wrong answers: " + wrongAs);
             //alert("Answer is wrong!");
-            $(questions[j]).html("<p>You were wrong!</p><img src='assets/images/crystal2.jpg'/>")
-            replaceFunction();
-            setTimeout(showNext, 2000)
+            $(questions[j]).html("<h4>Wrong answer! The correct answer is: " + answers[j] + "</h4><img src=" + images[j] + " width='300px'>")
+            setTimeout(replaceFunction, 2000);
+            setTimeout(showNext, 3000);
         }
     })
-    function replaceFunction(){
+    function replaceFunction() {
         $(questions[j]).replaceWith($(questions[j + 1]));
     }
     function showNext() {
         $(questions[j + 1]).show();
         j++
-        number = 10;
+        number = 15;
+        runTimer();
     }
     function runTimer() {
         clearInterval(intervalId);
@@ -62,10 +67,13 @@ $(document).ready(function () {
 
         if (number === 0) {
 
-            stop();
-            alert("time is up!");
+            //stop();
+            //alert("time is up!");
             wrongAs++;
-            console.log("wrong answers: " + wrongAs);
+            //console.log("wrong answers: " + wrongAs);
+            $(questions[j]).html("<h4>Your time is up! The correct answer was: " + answers[j] + "</h4><img src=" + images[j] + " width='300px'>")
+            setTimeout(replaceFunction, 2000);
+            setTimeout(showNext, 3000);
 
         }
     }
